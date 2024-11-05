9ef0646db169cbc3cedbd9c22d2a1d13aebb64ba
“Patricio <adaropato2@gmail.com>
2024-11-04 21:38:06 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -19,13 +19,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA2N6LgiVmRiN4MJ-AIUuyFFyAhM_pPUNM",
-  authDomain: "ecomerce-reactapp-5047a.firebaseapp.com",
-  projectId: "ecomerce-reactapp-5047a",
-  storageBucket: "ecomerce-reactapp-5047a.appspot.com",
-  messagingSenderId: "165941261098",
-  appId: "1:165941261098:web:01814ab20984ee3e98a453"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:  process.env.REACT_APP_PROYECT_ID,
+  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_MESSAGING_SENDER_ID,
+  appId:  process.env.REACT_APP_APP_ID,
 };
+console.log(firebaseConfig);
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
