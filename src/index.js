61aef3acde8deb62df4ad9cfb2e67150dc81cae7
“Patricio <adaropato2@gmail.com>
2024-11-05 22:49:13 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -54,15 +54,13 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-  // <React.StrictMode>
+  <React.StrictMode>
     <CartContextProvider value={[]} >
         <RouterProvider router={router} />
     </CartContextProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
